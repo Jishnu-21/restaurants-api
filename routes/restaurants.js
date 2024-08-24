d1d042019a6f656be4f05981c7b5d832f4402d3d
Jishnu <jpjishnu21@gmail.com>
2024-08-24 22:42:12 +0530

    (fix) Added authenticate middleware to routes

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const { getAllRestaurants,createRestaurants, getRestaurant,deleteRestaurant, updateRestaurant } = require('../controllers/restaurantController')
 const router = express.Router()
+const authenticate = require('../middleware/authMiddleware');
 
 
-router.get('/',getAllRestaurants)
-router.post('/',createRestaurants)
-router.get('/:id',getRestaurant)
-router.put('/:id',updateRestaurant)
-router.delete('/:id',deleteRestaurant)
+router.get('/',authenticate,getAllRestaurants)
+router.post('/',authenticate,createRestaurants)
+router.get('/:id',authenticate,getRestaurant)
+router.put('/:id',authenticate,updateRestaurant)
+router.delete('/:id',authenticate,deleteRestaurant)
 
 module.exports = router
